1bae420e742ed3beb7d43b7dcd6a39a410cf5e49
TWQ1765 <www.taaadaaa@qq.com>
2019-06-27 18:51:27 +0800

    added c.file and h.file.

---
+++ src/tryFake.h
@@ -0,0 +1,5 @@
+#ifndef _TRYFAKE_H
+#define _TRYFAKE_H
+
+
+#endif // _TRYFAKE_H
---
+++ test/test_tryFake.c
@@ -0,0 +1,15 @@
+#include "unity.h"
+#include "tryFake.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_tryFake_NeedToImplement(void)
+{
+    TEST_IGNORE_MESSAGE("Need to Implement tryFake");
+}
